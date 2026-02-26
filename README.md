1aae55a92bbf40408e5e083003c6c33c6ae191e5
Francho19 <Frankjosemirandabeleno331@gmail.com>
2026-02-25 20:29:01 -0500

    Proyecto es para reparar conceptos de HTML, CSS Y JS.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Estructura-De--Datos-desarrollo-
 Repositorios para guardar los proyectos realizados en clases.
+
+# mi-proyecto
+Este proyecto es para reparar conceptos de HTML, CSS Y JS.
---
+++ script.js
@@ -0,0 +1,10 @@
+const btn= document.getElementById("themeBtn");
+
+btn.addEventListener( "click", ()=>{
+document.body.classList.toggle("dark");
+if(document.body.classList.contains("dark")){
+    btn.textContent= "Modo claro";
+}else{
+    btn.textContent = "Modo oscuro";
+}
+});
