d40a2f9053d70b785e05763e9eb266c81a44be3b
Frank <Frankjosemirandabeleno331@gmail.com>
2026-03-04 17:18:49 -0500

    Trabajo con el trabajo modificado con Reto Extra

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-const btn= document.getElementById("themeBtn");
+const btn = document.getElementById("themeBtn");
 
-btn.addEventListener( "click", ()=>{
-document.body.classList.toggle("dark");
-if(document.body.classList.contains("dark")){
-    btn.textContent= "Modo claro";
-}else{
-    btn.textContent = "Modo oscuro";
+// Cargar tema guardado
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark") {
+    document.body.classList.add("dark");
+    btn.innerHTML = "☀️ Modo claro";
+} else {
+    btn.innerHTML = "🌙 Modo oscuro";
 }
+
+btn.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        btn.innerHTML = "☀️ Modo claro";
+        localStorage.setItem("theme", "dark");
+    } else {
+        btn.innerHTML = "🌙 Modo oscuro";
+        localStorage.setItem("theme", "light");
+    }
+
 });
